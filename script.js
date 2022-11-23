9709ccd39d1fa044e9bdad5a3d0e7e5c541a0d87
GalaxyShad <vadim.kadyrin@mail.ru>
2022-11-23 14:42:53 +0500

    feat: mode switch, reset

--- script.js
+++ script.js
@@ -93,155 +93,73 @@ nTapeBack.onscroll = viewUpdateTapeCurrElement;
 const turingSumProgram = () => {
   return {
     Q0: {
-      '1': { state: "Q1", out: "0", action: "R" },
+      1: { state: "Q1", out: "0", action: "R" },
       "=": { state: "Q3", out: "=", action: "R" },
       "+": { state: "Q0", out: "+", action: "R" },
-      '0': { state: "Q0", out: "0", action: "R" },
+      0: { state: "Q0", out: "0", action: "R" },
     },
 
     Q1: {
-      '1': { state: "Q1", out: "1", action: "R" },
+      1: { state: "Q1", out: "1", action: "R" },
       "=": { state: "Q1", out: "=", action: "R" },
       "+": { state: "Q1", out: "+", action: "R" },
-      '0': { state: "Q2", out: "1", action: "L" },
+      0: { state: "Q2", out: "1", action: "L" },
     },
 
     Q2: {
-      '1': { state: "Q2", out: "1", action: "L" },
+      1: { state: "Q2", out: "1", action: "L" },
       "=": { state: "Q2", out: "=", action: "L" },
       "+": { state: "Q2", out: "+", action: "L" },
-      '0': { state: "Q0", out: "0", action: "R" },
+      0: { state: "Q0", out: "0", action: "R" },
     },
 
     Q3: {
-      '1': { state: "Q3", out: "1", action: "S" },
+      1: { state: "Q3", out: "1", action: "S" },
       "=": { state: "Q3", out: "=", action: "S" },
       "+": { state: "Q3", out: "+", action: "S" },
-      '0': { state: "Q3", out: "0", action: "S" },
+      0: { state: "Q3", out: "0", action: "S" },
     },
   };
 };
 
-const turingMultiplyProgram = () => {
+const turingMultProgram = () => {
   return {
     Q0: {
-      '0': { state: "Q0", out: "0", action: "R" },
-      "2": { state: "Q0", out: "2", action: "R" },
-      "=": { state: "Q0", out: "=", action: "R" },
-      '1': { state: "Q0", out: "1", action: "R" },
-      'x': { state: "Q1", out: "x", action: "R" },
+      1: { state: "Q1", out: "0", action: "R" },
+      "=": { state: "Q3", out: "=", action: "L" },
+      "*": { state: "Q4", out: "0", action: "R" },
+      0: { state: "Q0", out: "0", action: "R" },
     },
 
     Q1: {
-      '1': { state: "Q2", out: "2", action: "R" },
-      "2": { state: "Q1", out: "2", action: "R" },
-      "0": { state: "Q1", out: "0", action: "R" },
-      '=': { state: "Q1", out: "=", action: "R" },
-      'x': { state: "Q1", out: "x", action: "R" },
+      1: { state: "Q1", out: "1", action: "R" },
+      "=": { state: "Q1", out: "=", action: "R" },
+      "*": { state: "Q0", out: "*", action: "R" },
+      0: { state: "Q2", out: "1", action: "L" },
     },
 
     Q2: {
-      '1': { state: "Q2", out: "1", action: "L" },
-      "2": { state: "Q2", out: "2", action: "L" },
+      1: { state: "Q2", out: "1", action: "L" },
       "=": { state: "Q2", out: "=", action: "L" },
-      'x': { state: "Q3", out: "x", action: "L" },
-      '0': { state: "Q2", out: "0", action: "R" },
+      0: { state: "Q0", out: "0", action: "R" },
     },
 
     Q3: {
-      '1': { state: "Q4", out: "2", action: "R" },
-      "2": { state: "Q3", out: "2", action: "L" },
-      "0": { state: "Q6", out: "0", action: "R" },
-      'x': { state: "Q6", out: "x", action: "R" },
-      '=': { state: "Q6", out: "=", action: "R" },
+      "*": { state: "Q2", out: "*", action: "L" },
+      0: { state: "Q3", out: "1", action: "L" },
     },
-    
+
     Q4: {
-      'x': { state: "Q4", out: "x", action: "R" },
-      "2": { state: "Q4", out: "2", action: "R" },
-      "=": { state: "Q4", out: "=", action: "R" },
-      '1': { state: "Q4", out: "1", action: "R" },
-      '0': { state: "Q5", out: "1", action: "R" },
-    },
-    
-    Q5: {
-      '1': { state: "Q2", out: "1", action: "L" },
-      "2": { state: "Q2", out: "2", action: "L" },
-      "0": { state: "Q2", out: "0", action: "L" },
-      '=': { state: "Q2", out: "=", action: "L" },
-      'x': { state: "Q2", out: "x", action: "L" },
-    },
-    
-    Q6: {
-      '2': { state: "Q6", out: "1", action: "R" },
-      "x": { state: "Q7", out: "x", action: "R" },
-      "1": { state: "Q6", out: "1", action: "R" },
-      '0': { state: "Q6", out: "0", action: "R" },
-      '=': { state: "Q6", out: "=", action: "R" },
-    },
-    
-    Q7: {
-      '2': { state: "Q7", out: "2", action: "R" },
-      "1": { state: "Q2", out: "2", action: "R" },
-      "=": { state: "Q8", out: "=", action: "L" },
-      '0': { state: "Q7", out: "0", action: "R" },
-      'x': { state: "Q7", out: "x", action: "R" },
-    },
-
-    Q8: {
-      '2': { state: "Q8", out: "1", action: "L" },
-      "x": { state: "Q9", out: "x", action: "S" },
-      "1": { state: "Q8", out: "1", action: "R" },
-      '0': { state: "Q8", out: "0", action: "R" },
-      '=': { state: "Q8", out: "=", action: "R" },
+      1: { state: "Q4", out: "0", action: "R" },
+      "=": { state: "Q4", out: "0", action: "S" },
+      0: { state: "Q4", out: "0", action: "S" },
     },
   };
 };
 
-
-const turingAlexMultProgram = () => {
-  return {
-    Q0: {
-      '1': { state: "Q1", out: "0", action: "R" },
-      "=": { state: "Qm", out: "=", action: "L" },
-      "*": { state: "Qd", out: "0", action: "R" },
-      '0': { state: "Q0", out: "0", action: "R" },
-    },
-
-    Q1: {
-      '1': { state: "Q1", out: "1", action: "R" },
-      "=": { state: "Q1", out: "=", action: "R" },
-      "*": { state: "Q0", out: "*", action: "R" },
-      '0': { state: "Qb", out: "1", action: "L" },
-    },
-
-    Qb: {
-      '1': { state: "Qb", out: "1", action: "L" },
-      "=": { state: "Qb", out: "=", action: "L" },
-      //"*": { state: "Q2", out: "+", action: "L" },
-      '0': { state: "Q0", out: "0", action: "R" },
-    },
-
-    Qm: {
-      //'1': { state: "Q3", out: "1", action: "S" },
-      //"=": { state: "Q3", out: "=", action: "S" },
-      "*": { state: "Qb", out: "*", action: "L" },
-      '0': { state: "Qm", out: "1", action: "L" },
-    },
-
-    Qd: {
-      '1': { state: "Qd", out: "0", action: "R" },
-      "=": { state: "Qd", out: "0", action: "S" },
-      //"*": { state: "Q3", out: "+", action: "S" },
-      '0': { state: "Qd", out: "0", action: "S" },
-    },
-  };
-};
-
-
-// const turingProgram = turingSumProgram();
+let turingProgram = turingSumProgram();
 // const turingProgram = turingMultiplyProgram();
-const turingProgram = turingAlexMultProgram();
+// const turingProgram = turingMultProgram();
 
 const turingIterate = ([roll, cur = 0, state = "Q0"]) => {
   const c = roll[cur];
@@ -272,10 +190,15 @@ const btnIterate = document.querySelector("#btn-iterate");
 const btnMode = document.querySelector("#btn-mode");
 
 let sumModeFlag = true;
-btnMode.onclick = () =>
-  (btnMode.innerHTML = (sumModeFlag = !sumModeFlag)
+btnMode.onclick = () => {
+  sumModeFlag = !sumModeFlag;
+
+  btnMode.innerHTML = sumModeFlag
     ? "Summation Mode"
-    : "Multiply Mode");
+    : "Multiply Mode";
+
+  turingProgram = sumModeFlag ? turingSumProgram() : turingMultProgram();
+};
 
 updateView({
   values: "000000000000000000000000000000",
@@ -291,7 +214,27 @@ btnIterate.onclick = () => {
   view.state = turing[2];
 
   updateView(view);
-
-  console.log(view.state);
-  updateView(view);
 };
+
+document.querySelector('#btn-iterate-100').onclick = () => {
+
+  for (let i = 0; i < 100; i++) {
+    view = getView();
+
+    let turing = turingIterate([view.values, view.cursor, view.state]);
+    view.values = turing[0];
+    view.cursor = turing[1];
+    view.state  = turing[2];
+
+    updateView(view);
+  }
+  
+}
+
+document.querySelector('#btn-reset').onclick = () => {
+  updateView({
+    values: "000000000000000000000000000000",
+    cursor: 0,
+    state: "Q0",
+  });
+}
